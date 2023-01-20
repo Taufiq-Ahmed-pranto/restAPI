b11f6a9424351b23d9f481f34660262f6b194eae
taufiq-ahmed <taufiqahmed806@gmail.com>
2023-01-20 22:17:51 +0600

    second commit

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const Student = require("./model/studentModel");
 const app = express();
 app.use(express.json());
 
+const port = process.env.PORT || 5000;
+
 //connect to mongodb
 
-mongoose.connect(process.env.LINK, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
-    console.log("connected to mongodb");
-}).catch(err => {
-    console.log(err);
-});
-
-
-
-
-
+mongoose
+    .connect(process.env.LINK, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("connected to mongodb");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", async(req, res) => {
-    res.send("Hello web hosting")
-})
+    res.send("Hello web hosting");
+});
 
 app.get("/get-student-data", async(req, res) => {
-
     try {
         const data = await Student.find();
 
         res.status(200).json({
             status: "ok",
             data: data,
-        })
+        });
     } catch (err) {
         console.log(err);
     }
-
-})
-
+});
 
 app.post("/create-student", async(req, res) => {
     try {
@@ -47,50 +47,44 @@ app.post("/create-student", async(req, res) => {
         res.status(201).json({
             status: "ok",
             data: data,
-        })
+        });
     } catch (error) {
         console.log(error);
     }
-})
+});
 
 app.patch("/update-student/:id", async(req, res) => {
-
     try {
         const id = req.params.id;
         const body = req.body;
 
-        const result = await Student.findOneAndUpdate({ _id: id }, body, { new: true });
+        const result = await Student.findOneAndUpdate({ _id: id }, body, {
+            new: true,
+        });
 
         res.status(200).json({
             status: "ok",
             data: result,
-        })
+        });
     } catch (error) {
         console.log(err);
     }
-
-
-
-})
+});
 
 app.delete("/delete-student/:id", async(req, res) => {
-
     try {
         const id = req.params.id;
         const result = await Student.deleteOne({ _id: id });
 
         res.status(200).json({
             status: "ok",
-            message: "Student has been deleted"
-        })
-
+            message: "Student has been deleted",
+        });
     } catch (error) {
         console.log(error);
     }
-})
-
-
+});
 
 app.listen(8000, () => {
-    console.log("server run at port 8000");
-})
+    console.log("server run at port " + port);
+});
